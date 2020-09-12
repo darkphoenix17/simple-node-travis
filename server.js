067412fb644e2d58807e089633742a51e236ee19
darkphoenix17 <56074105+darkphoenix17@users.noreply.github.com>
2020-09-12 11:21:17 +0530

    updated

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const favoriteFood = process.env.FAVORITE_FOOD;
+// Set a local variable populated by the environment variable $NAME
+const FAVORITE_INSTRUCTOR = process.env.NAME;
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
-  while (true) {
-    console.log("Microservices rock!");
-    console.log(`My favorite food is ${favoriteFood}`);
+  while(true) {
+    console.log(`${FAVORITE_INSTRUCTOR} rocks!`);
     await sleep(5000);
   }
 }
